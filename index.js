d4fdc177d37ada073cff9161b775e735b693e459
Sagar-dots <145258938+Sagar-dots@users.noreply.github.com>
2023-10-26 07:39:42 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,18 @@
-// JavaScript code for handling the start game logic
-const carouselItems = document.querySelectorAll('.carousel-item');
-const startButton = document.getElementById('start-game');
-
-let selectedTechnology = null;
-
-// Function to update the selected technology
-function selectTechnology(tech) {
-    selectedTechnology = tech;
-    startButton.style.display = 'block';
-    startButton.textContent = `Start Game for ${tech}`;
-}
-
 const carousel = document.querySelector('.carousel');
 const carouselItems = document.querySelectorAll('.carousel-item');
-const startButton = document.getElementById('start-game');
-
-let selectedTechnology = null;
-
-function selectTechnology(tech) {
-    selectedTechnology = tech;
-    startButton.style.display = 'block';
-    startButton.textContent = `Start Game for ${tech}`;
-}
 
 carouselItems.forEach((item) => {
     item.addEventListener('click', () => {
-        carouselItems.forEach((el) => el.classList.remove('active'));
-        item.classList.add('active');
-        selectTechnology(item.getAttribute('data-tech'));
+        // Redirect to the respective HTML pages
+        const tech = item.getAttribute('data-tech');
+        window.location.href = `${tech}.html`;
     });
-});
 
-startButton.addEventListener('click', () => {
-    if (selectedTechnology) {
-        window.location.href = `game.html?tech=${selectedTechnology}`;
-    }
-});
-
-
-// Event listener for carousel item selection
-carouselItems.forEach((item) => {
-    item.addEventListener('click', () => {
-        carouselItems.forEach((el) => el.classList.remove('active'));
-        item.classList.add('active');
-        selectTechnology(item.getAttribute('data-tech'));
+    const startButton = item.querySelector('.start-button');
+    item.addEventListener('mouseover', () => {
+        startButton.style.display = 'block';
     });
-});
-
-// Event listener for the start game button
-startButton.addEventListener('click', () => {
-    // Redirect to the game page with the selected technology
-    if (selectedTechnology) {
-        window.location.href = `${selectedTechnology}.html`;
-    }
+    item.addEventListener('mouseleave', () => {
+        startButton.style.display = 'none';
+    });
 });
