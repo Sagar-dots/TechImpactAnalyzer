328f432197033a6c63d26fec69123026353a5189
Sagar-dots <145258938+Sagar-dots@users.noreply.github.com>
2023-10-26 08:55:11 +1100

    Update quantum-script.js

--- quantum-script.js
+++ quantum-script.js
@@ -1,88 +1,88 @@
-const questions = [
+const scenarios = [
     {
-        question: "Your company is developing a quantum computer. How do you ensure data security?",
+        question: "Scenario 1: Secure Communications",
+        description: "You are working on implementing quantum computing to enhance secure communications. Quantum encryption has the potential to revolutionize data security. What approach will you take?",
         options: [
             {
-                text: "Implement quantum encryption for data security",
+                text: "Option 1: Focus on quantum encryption development.",
                 points: 5,
-                impact: "Implementing quantum encryption ensures the highest level of data security.",
-                positive: true
+                impact: "Focusing on quantum encryption development strengthens data security."
             },
             {
-                text: "Use traditional encryption methods",
+                text: "Option 2: Collaborate with experts to refine the encryption technology.",
+                points: 3,
+                impact: "Collaboration with experts accelerates encryption technology refinement."
+            },
+            {
+                text: "Option 3: Prioritize testing quantum communication networks.",
                 points: 2,
-                impact: "Using traditional encryption methods offers good security but not quantum-proof.",
-                positive: true
-            },
-            {
-                text: "No encryption measures",
-                points: 1,
-                impact: "Lack of encryption measures may lead to data breaches and security issues.",
-                positive: false
+                impact: "Prioritizing testing helps in identifying network vulnerabilities."
             }
         ]
     },
     {
-        question: "Your quantum computer can break existing encryption methods. How do you handle this capability?",
+        question: "Scenario 2: Quantum Computing Ethics",
+        description: "You are responsible for addressing ethical considerations related to quantum computing. Ethical concerns include potential misuse and consequences of quantum computing. What will you do?",
         options: [
             {
-                text: "Work with governments to establish responsible use guidelines",
+                text: "Option 1: Establish ethical guidelines and policies.",
                 points: 5,
-                impact: "Working with governments to establish guidelines ensures responsible and safe use.",
-                positive: true
+                impact: "Establishing guidelines and policies ensures responsible quantum computing."
             },
             {
-                text: "Sell the technology without restrictions",
-                points: 2,
-                impact: "Selling the technology without restrictions raises concerns about misuse and security.",
-                positive: false
+                text: "Option 2: Collaborate with ethicists and industry leaders to set ethical standards.",
+                points: 4,
+                impact: "Collaboration with experts strengthens ethical standards."
             },
             {
-                text: "No specific plan",
-                points: 1,
-                impact: "Lack of a plan may lead to uncontrolled use and potential risks.",
-                positive: false
+                text: "Option 3: Conduct public awareness campaigns about quantum ethics.",
+                points: 3,
+                impact: "Public awareness campaigns promote responsible quantum technology usage."
             }
         ]
     },
     {
-        question: "Your quantum computer requires extremely low temperatures. How do you address environmental concerns?",
+        question: "Scenario 3: Quantum Computing Collaboration",
+        description: "You have the opportunity to partner with quantum computing experts from around the world. Collaboration can accelerate advancements but may also involve complex negotiations. What's your decision?",
         options: [
             {
-                text: "Implement efficient cooling systems and minimize energy consumption",
+                text: "Option 1: Collaborate with experts to drive quantum research and development.",
                 points: 5,
-                impact: "Efficient cooling systems and energy minimization reduce environmental impact.",
-                positive: true
+                impact: "Collaboration accelerates research and development."
             },
             {
-                text: "Use standard cooling methods and energy sources",
-                points: 2,
-                impact: "Standard cooling and energy sources lead to higher energy consumption and costs.",
-                positive: false
+                text: "Option 2: Form strategic alliances for specific projects.",
+                points: 4,
+                impact: "Strategic alliances allow for project-specific expertise."
             },
             {
-                text: "No environmental measures",
-                points: 1,
-                impact: "Lack of environmental measures negatively impacts the environment.",
-                positive: false
+                text: "Option 3: Continue with in-house development and research.",
+                points: 3,
+                impact: "In-house development maintains full control but may limit resources."
             }
         ]
     }
 ];
 
 const questionsContainer = document.getElementById("questions-container");
-const endGameButton = document.getElementById("end-game-button");
+const impactContainer = document.getElementById("impact-container");
+const showScoreButton = document.getElementById("show-score-button");
+const goBackButton = document.getElementById("go-back-button");
 const totalPointsContainer = document.getElementById("total-points");
+
 let userPoints = 0;
 let questionIndex = 0;
 
 function renderQuestion() {
-    if (questionIndex < questions.length) {
-        const currentQuestion = questions[questionIndex];
+    if (questionIndex < scenarios.length) {
+        const currentScenario = scenarios[questionIndex];
         const questionElement = document.createElement("div");
-        questionElement.classList.add("question");
-        questionElement.innerHTML = `<p>${currentQuestion.question}</p>`;
-        currentQuestion.options.forEach((option, index) => {
+        questionElement.classList.add("scenario");
+        questionElement.innerHTML = `
+            <h2>${currentScenario.question}</h2>
+            <p>${currentScenario.description}</p>
+        `;
+        currentScenario.options.forEach((option, index) => {
             const optionButton = document.createElement("button");
             optionButton.textContent = option.text;
             optionButton.addEventListener("click", () => handleOptionClick(option, index));
@@ -90,30 +90,39 @@ function renderQuestion() {
         });
         questionsContainer.appendChild(questionElement);
     } else {
-        endGame();
+        showScoreButton.style.display = "block";
     }
 }
 
 function handleOptionClick(option, index) {
     userPoints += option.points;
+    showImpact(option.impact);
     questionIndex++;
-    renderImpact(option.impact, option.positive);
-    renderQuestion();
+
+    if (questionIndex < scenarios.length) {
+        renderQuestion();
+    }
 }
 
-function renderImpact(impact, isPositive) {
-    const impactElement = document.createElement("p");
-    impactElement.textContent = impact;
-    impactElement.classList.add(isPositive ? "positive-impact" : "negative-impact");
-    questionsContainer.appendChild(impactElement);
+function showImpact(impactText) {
+    const impactElement = document.createElement("div");
+    impactElement.classList.add("impact");
+    impactElement.textContent = impactText;
+    impactContainer.appendChild(impactElement);
 }
 
-function endGame() {
-    questionsContainer.style.display = "none";
-    endGameButton.style.display = "none";
-    totalPointsContainer.style.display = "block";
+function showScore() {
     totalPointsContainer.textContent = `Total Points: ${userPoints}`;
+    totalPointsContainer.style.display = "block";
+    showScoreButton.style.display = "none";
+    goBackButton.style.display = "block";
 }
+
+function goBack() {
+    window.location.href = "index.html"; // Replace with the URL of your main page
+}
+
+showScoreButton.addEventListener("click", showScore);
+goBackButton.addEventListener("click", goBack);
 
 renderQuestion();
-endGameButton.addEventListener("click", endGame);
