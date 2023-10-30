42af21d6752d93813f94e082985e632c47ee0bc4
Sagar-dots <145258938+Sagar-dots@users.noreply.github.com>
2023-10-30 15:52:06 +1100

    Update Neuralink.js

--- Neuralink.js
+++ Neuralink.js
@@ -1,75 +1,69 @@
-// JavaScript logic for Neuralink scenario
-// JavaScript logic for Neuralink scenario
 const scenarios = [
     {
-        question: "Scenario 1: User Data Privacy",
-        description: " Imagine that you are a influential part of Neuralink Project's Developement. Neuralink is preparing to release a new brain-computer interface. How do you think you can ensure user data privacy while adhering to responsible innovation KPIs?",
+        question: "Scenario 1: Joining the Collaborative Brain-Computer Interface (BCI) Research Team",
+        description: "Background: You are presented with an opportunity to join Neuralink's cutting-edge Brain-Computer Interface (BCI) research team. Neuralink aims to reshape human interaction with technology through BCIs. How do you, as a key decision-maker, ensure ethical research and uphold gender and ethnic inclusivity in your new role?",
         options: [
             {
-                text: "Option 1: Implement strong encryption and strict data access controls, focusing on strcit policies against breaches in project participation.",
+                text: "Option 1: Accept the position, actively collaborating with esteemed neuroscientists and promoting gender and ethnic diversity within the team, while committing to strict ethical guidelines.",
                 points: 5,
-                impact: "Scenario 1, Option 1: I would impose strong encryption and strict data access controls ensure user data privacy while promoting policies in research and management."
+                impact: "You join the team, prioritizing inclusivity, collaboration, and ethics. (Scenario 1, Option 1)"
             },
             {
-                text: "Option 2:  I would collect minimal data but without encryption, actively integrating gender dimensions in research and innovation outcomes.",
-                points: 3,
-                impact: "Scenario 1 Option 2: Minimal data  without encryption combined with the active integration of gender dimensions, promoting ethical innovation."
-            },
-            {
-                text: "Option 3:  I think no data collection and no privacy measures is good enough for now.",
-                points: 2,
-                impact: "Scenario 1 Option 3:Lack of data collection and privacy measures negatively impacts user experience but includes efforts to eliminate gender-related barriers."
-            }
-        ]
-    },
-    {
-        question: "Scenario 2: Collaborative Research",
-        description: "You have the choice to partner with leading neuroscience institutions for collaborative research. How do you proceed with the gender and ethnic insclusions in these instutions?",
-        options: [
-            {
-        question: "Scenario 1: Collaborative Brain-Computer Interface (BCI) Research",
-        description: "Neuralink is embarking on a groundbreaking project to develop advanced Brain-Computer Interfaces (BCIs) for various applications. How do you approach collaborative research to ensure inclusivity and ethical standards?",
-        options: [
-            {
-                text: "Option 1: Collaborate with leading neuroscientists and research institutions worldwide, actively promoting gender and ethnic diversity within the research teams. Ensure research protocols adhere to strict ethical guidelines.",
-                points: 5,
-                impact: "Collaborating with diverse experts and promoting inclusivity strengthens research excellence while upholding ethical standards."
-            },
-            {
-                text: "Option 2: Allow each research institution to decide on their collaboration approach, emphasizing the importance of inclusivity and ethical considerations in the research process.",
+                text: "Option 2: Emphasize the importance of inclusivity and ethical considerations but grant research institutions the autonomy to decide on their collaboration approach.",
                 points: 4,
-                impact: "Option 2: Granting autonomy to institutions respects their choices, emphasizing inclusivity and ethics."
+                impact: "You encourage inclusivity and ethics but respect institutions' choices. (Scenario 1, Option 2)"
             },
             {
-                text: "Option 3: Maintain a closed-door approach with limited collaborations, prioritizing internal expertise, and regularly organizing group deliberations on societal, gender, and ethical aspects.",
+                text: "Option 3: Decide to stay cautious, limiting external collaborations and prioritizing internal expertise while organizing regular discussions on societal, gender, and ethical aspects.",
                 points: 3,
-                impact: "Option 3: Limited external collaboration maintains control while internal deliberations focus on societal, gender, and ethical dimensions."
+                impact: "You opt for a more guarded approach, focusing on internal discussions and maintaining control. (Scenario 1, Option 3)"
             }
         ]
     },
-        
     {
-        question: "Scenario 3: Accessibility and Pricing",
-        description: "You plan to make Neuralink accessible to a broad audience. How do you address affordability while considering environmental and social sustainability?",
+        question: "Scenario 2: Addressing Public Ethical Oversight",
+        description: "Background: In your new role at Neuralink, you're confronted with growing public scrutiny and ethical concerns related to the project. You need to navigate the balancing act between ensuring public ethical oversight and maintaining project momentum. How do you handle this ethical challenge?",
         options: [
             {
-                text: "Option 1: Offer affordable pricing and subsidies to reach a wider population, considering environmental and social sustainability.",
+                text: "Option 1: Champion the creation of an independent ethical review board that includes diverse experts and public representatives. Actively involve them in decision-making processes and commit to transparently sharing research outcomes.",
                 points: 5,
-                impact: "Scenario 3 Option 1:Affordable pricing and subsidies enhance accessibility while promoting environmental and social sustainability."
+                impact: "You lead the charge in establishing an independent review board and embrace transparent, inclusive decision-making. (Scenario 2, Option 1)"
             },
             {
-                text: "Option 2: Set a premium price for exclusivity. Actively communicate research results within stakeholder networks.",
+                text: "Option 2: Engage in frequent public town hall meetings and surveys to gather public opinions, ensuring transparency and inclusivity in project decisions.",
+                points: 4,
+                impact: "You advocate for regular public engagement, promoting transparency and inclusive decision-making while respecting public concerns. (Scenario 2, Option 2)"
+            },
+            {
+                text: "Option 3: Prioritize project progress by limiting public engagement and organizing internal discussions on ethical issues and societal dimensions to streamline project advancement.",
                 points: 3,
-                impact: "Scenario 3 Option 2:A premium price may limit accessibility but ensures exclusivity, with active communication of results for societal benefits."
+                impact: "You choose to focus on accelerating project progress, which may affect transparency and inclusivity, emphasizing research and development. (Scenario 2, Option 3)"
+            }
+        ]
+    },
+    {
+        question: "Scenario 3: Paving the Way for Accessible BCI Technology",
+        description: "Background: Neuralink's vision is to make Brain-Computer Interface (BCI) technology accessible to a diverse population. You are tasked with ensuring affordability, equitable access, and social inclusivity. How do you, personally, balance premium pricing for research and development with making BCI technology accessible to all?",
+        options: [
+            {
+                text: "Option 1: Advocate for a range of pricing options, including subsidies and grants, to ensure affordability for various demographics, with a strong focus on inclusivity and equity.",
+                points: 5,
+                impact: "You lead the effort to offer diverse pricing options, subsidies, and grants to make BCI technology accessible and promote social inclusivity and equity. (Scenario 3, Option 1)"
             },
             {
-                text: "Option 3: Not focused on affordability, prioritize innovation and resource use efficiency.",
-                points: 2,
-                impact: "Scenario 3 Option 3:Lack of affordability focus may limit market reach but spurs innovation and resource use efficiency."
+                text: "Option 2: Support a competitive pricing model and actively engage with community organizations, allowing for feedback and promoting outreach programs to broaden access.",
+                points: 4,
+                impact: "You back a competitive pricing model and community engagement to expand access and inclusivity, fostering outreach and public collaboration. (Scenario 3, Option 2)"
+            },
+            {
+                text: "Option 3: Prioritize innovation and research and development with premium pricing, with limited outreach programs, focusing on advancement and progress.",
+                points: 3,
+                impact: "You opt to prioritize innovation and research and development, which may limit accessibility and inclusivity. (Scenario 3, Option 3)"
             }
         ]
     }
 ];
+
 
 
 
