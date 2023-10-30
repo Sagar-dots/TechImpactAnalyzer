7714d87ac35df608e3c2b53360a7f30365f3ec32
Sagar-dots <145258938+Sagar-dots@users.noreply.github.com>
2023-10-30 15:15:44 +1100

    Update SelfDrivingCars.js

--- SelfDrivingCars.js
+++ SelfDrivingCars.js
@@ -7,17 +7,17 @@ const scenarios = [
             {
                 text: "Option 1: Implement AI algorithms that prioritize the safety of passengers and pedestrians in all situations, even if it means sacrificing the passengers' lives in extreme cases.",
                 points: 5,
-                impact: "Safety-first algorithms prioritize the well-being of all, aligning with ethical values."
+                impact: "Scenario 1 Option 1:Safety-first algorithms prioritize the well-being of all, aligning with ethical values."
             },
             {
                 text: "Option 2: Develop AI algorithms that protect passengers at all costs, even if it means causing harm to pedestrians, prioritizing passenger safety over all else.",
                 points: 3,
-                impact: "Passenger-centric algorithms may raise ethical concerns about the safety of others."
+                impact: "Scenario 1 Option 2:Passenger-centric algorithms may raise ethical concerns about the safety of others."
             },
             {
                 text: "Option 3: Leave ethical decisions to the passengers by offering an option to choose the vehicle's behavior in case of an accident, potentially causing moral dilemmas and risks.",
                 points: 2,
-                impact: "Passenger choice may raise moral dilemmas and ethical complexities."
+                impact: "Scenario 1 Option 3:Passenger choice may raise moral dilemmas and ethical complexities."
             }
         ]
     },
@@ -28,17 +28,17 @@ const scenarios = [
             {
                 text: "Option 1: Implement strong encryption and data anonymization techniques to protect user data privacy, adhering to strict ethical standards.",
                 points: 5,
-                impact: "Strong data protection measures align with ethical standards and safeguard user privacy."
+                impact: "Scenario 2 Option 1:Strong data protection measures align with ethical standards and safeguard user privacy."
             },
             {
                 text: "Option 2: Collect and store user data with minimal protection, potentially compromising user privacy and raising ethical concerns.",
                 points: 2,
-                impact: "Minimal data protection may raise ethical concerns about data privacy."
+                impact: "Scenario 2 Option 2:Minimal data protection may raise ethical concerns about data privacy."
             },
             {
                 text: "Option 3: Share user data with third parties without consent, disregarding ethical principles and privacy norms.",
                 points: 1,
-                impact: "Sharing data without consent violates ethical standards and privacy norms."
+                impact: "Scenario 2 Option 3:Sharing data without consent violates ethical standards and privacy norms."
             }
         ]
     },
@@ -49,17 +49,17 @@ const scenarios = [
             {
                 text: "Option 1: Invest in retraining programs and provide support for displaced workers to mitigate job loss, prioritizing ethical responsibility and societal well-being.",
                 points: 5,
-                impact: "Retraining programs and support demonstrate ethical responsibility and address job displacement concerns."
+                impact: "Scenario 3 Option 1:Retraining programs and support demonstrate ethical responsibility and address job displacement concerns."
             },
             {
                 text: "Option 2: Continue self-driving car deployment without addressing job displacement, potentially causing public outrage and ethical concerns.",
                 points: 2,
-                impact: "Failure to address job displacement may lead to public outrage and ethical issues."
+                impact: "Scenario 3 Option 2:Failure to address job displacement may lead to public outrage and ethical issues."
             },
             {
                 text: "Option 3: Prioritize automation and cost savings over job concerns, disregarding the ethical implications of job displacement.",
                 points: 1,
-                impact: "Prioritizing automation without addressing job displacement may raise ethical concerns."
+                impact: "Scenario 3 Option 3:Prioritizing automation without addressing job displacement may raise ethical concerns."
             }
         ]
     }
