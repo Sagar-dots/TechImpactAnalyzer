17449242287521aa4c709a2def05b54e0935d855
Sagar-dots <145258938+Sagar-dots@users.noreply.github.com>
2023-10-30 15:12:14 +1100

    Update CRISPRCase9.js

--- CRISPRCase9.js
+++ CRISPRCase9.js
@@ -1,43 +1,43 @@
 const scenarios = [
     {
-        question: "Scenario 1: Genetic Disease Editing",
-        description: "You are involved in a project using CRISPR-Cas9 to edit genes and potentially eliminate genetic diseases. How do you ensure responsible innovation and address gender equality?",
+        question: "Scenario 1: Ethical Gene Editing",
+        description: "Your team is pioneering CRISPR-Cas9 gene editing to cure genetic diseases. How do you ensure responsible innovation while addressing gender equality?",
         options: [
             {
-                text: "Option 1: Implement rigorous ethical guidelines and regulations to guide gene editing research, ensuring diversity and gender equality in the research team.",
+                text: "Option 1: Implement strict ethical guidelines and regulations, ensuring diverse gender representation in the research team.",
                 points: 5,
-                impact: "Rigorous ethical guidelines and diversity in the research team ensure responsible gene editing and gender equality in science."
+                impact: "Scenario 1, Option 1: Enforcing rigorous ethical guidelines and fostering diversity ensures responsible gene editing and gender equality."
             },
             {
-                text: "Option 2: Collaborate with ethicists, healthcare experts, and patient advocacy groups to establish ethical standards for gene editing, integrating gender dimensions into research and innovation.",
+                text: "Option 2: Collaborate with ethicists, healthcare experts, and patient advocacy groups to establish ethical standards for gene editing, with a focus on integrating gender dimensions.",
                 points: 4,
-                impact: "Collaboration with experts and gender-inclusive research promotes responsible gene editing and gender equality in healthcare innovation."
+                impact: "Scenario 1, Option 2: Collaboration with experts and inclusive research promotes responsible gene editing and gender equality in healthcare innovation."
             },
             {
-                text: "Option 3: Prioritize public awareness campaigns about gene editing ethics, involving diverse stakeholders, including the public, in the decision-making process.",
+                text: "Option 3: Prioritize public awareness campaigns about gene editing ethics, engaging diverse stakeholders, including the public, in the decision-making process.",
                 points: 3,
-                impact: "Public awareness campaigns promote responsible gene editing and diverse stakeholder engagement while raising gender equality awareness."
+                impact: "Scenario 1, Option 3: Public awareness campaigns promote responsible gene editing and diverse stakeholder involvement while raising awareness of gender equality."
             }
         ]
     },
     {
-        question: "Scenario 2: Environmental Impact",
+        question: "Scenario 2: Environmental Sustainability",
         description: "Your gene editing project may have an environmental impact due to genetic modifications. How do you ensure sustainability and inclusiveness?",
         options: [
             {
-                text: "Option 1: Actively include environmental values in the research and design process, considering ecological impact and involving diverse stakeholders.",
+                text: "Option 1: Actively integrate environmental values in the research and design process, considering ecological impact and involving diverse stakeholders.",
                 points: 5,
-                impact: "Environmental values and diverse stakeholder involvement ensure responsible gene editing and environmental sustainability."
+                impact: "Scenario 2, Option 1: Environmental values and diverse stakeholder involvement ensure responsible gene editing and environmental sustainability."
             },
             {
-                text: "Option 2: Focus on resource use efficiency and reduced waste generation during gene editing, collaborating with environmental experts, and inclusively involving diverse stakeholder viewpoints.",
+                text: "Option 2: Focus on resource use efficiency and waste reduction during gene editing, collaborating with environmental experts and including diverse stakeholder viewpoints.",
                 points: 4,
-                impact: "Resource efficiency, collaboration with environmental experts, and diverse stakeholder involvement promote sustainability and inclusiveness."
+                impact: "Scenario 2, Option 2: Resource efficiency, collaboration with environmental experts, and diverse stakeholder involvement promote sustainability and inclusiveness."
             },
             {
-                text: "Option 3: Continue without addressing environmental concerns, promoting innovation, but lacking in sustainability and inclusiveness.",
+                text: "Option 3: Continue without addressing environmental concerns, promoting innovation, but potentially lacking in sustainability and inclusiveness.",
                 points: 2,
-                impact: "Innovation may thrive, but the project might be criticized for a lack of sustainability and inclusiveness."
+                impact: "Scenario 2, Option 3: Innovation may thrive, but the project might face criticism for a lack of sustainability and inclusiveness."
             }
         ]
     },
@@ -48,21 +48,23 @@ const scenarios = [
             {
                 text: "Option 1: Promote open access to gene editing information, actively engage with the public, and collaborate with civil society groups and policy makers to ensure responsible innovation and societal acceptance.",
                 points: 5,
-                impact: "Open access, public engagement, and collaboration with various stakeholders ensure responsible gene editing and societal acceptance."
+                impact: "Scenario 3, Option 1: Open access, public engagement, and collaboration with various stakeholders ensure responsible gene editing and societal acceptance."
             },
             {
                 text: "Option 2: Use virtual platforms for data exchange, share research results with stakeholders, and actively communicate within the stakeholders' network to promote societal acceptance.",
                 points: 4,
-                impact: "Virtual platforms, sharing results, and stakeholder communication enhance transparency and societal acceptance of gene editing."
+                impact: "Scenario 3, Option 2: Virtual platforms, sharing results, and stakeholder communication enhance transparency and societal acceptance of gene editing."
             },
             {
                 text: "Option 3: Keep results confidential and prioritize innovation, risking public resistance and a lack of societal acceptance.",
                 points: 2,
-                impact: "Confidentiality may lead to public resistance and affect societal acceptance, focusing on innovation with potential risks."
+                impact: "Scenario 3, Option 3: Confidentiality may lead to public resistance and affect societal acceptance, focusing on innovation with potential risks."
             }
         ]
     }
+    // Add more scenarios as needed
 ];
+
 
 
 
